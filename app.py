4a7070e7714a8aa532360257f9663805afc464a2
kingmaks-sudo <mashuguikeita2001@gmail.com>
2026-08-29 01:14:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -846,6 +846,8 @@ def handle_disconnect():
             break
 
 
+import os
+
 if __name__ == "__main__":
-    print("Serveur lancé sur http://localhost:5000")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
